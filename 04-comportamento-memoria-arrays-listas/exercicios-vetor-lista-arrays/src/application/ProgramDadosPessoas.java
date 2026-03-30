7181551f61c8a94da32b4bdc25679d9a288517a7
RenatoDeFreitas <gprenatofreitas@gmail.com>
2026-03-30 08:21:08 -0300

    adicionado exercícios de listas

--- 04-comportamento-memoria-arrays-listas/exercicios-vetor-lista-arrays/src/application/ProgramDadosPessoas.java
+++ 04-comportamento-memoria-arrays-listas/exercicios-vetor-lista-arrays/src/application/ProgramDadosPessoas.java
@@ -1,5 +1,20 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ProgramDadosPessoas {
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner teclado = new Scanner(System.in);
+
+		System.out.print("How many people will be registered? \n");
+		
+		
+		
+		
+		teclado.close();
+	}
 
 }
--- 04-comportamento-memoria-arrays-listas/exercicios-vetor-lista-arrays/src/entities/PessoasDados.java
+++ 04-comportamento-memoria-arrays-listas/exercicios-vetor-lista-arrays/src/entities/PessoasDados.java
@@ -2,4 +2,45 @@ package entities;
 
 public class PessoasDados {
 
+	// ATRIBUTOS
+	private String nome;
+	private double altura;
+	private char genero;
+
+	// CONSTRUTOR
+
+	public PessoasDados(String nome, double altura, char genero) {
+		this.nome = nome;
+		this.altura = altura;
+		this.genero = genero;
+	}
+
+	// GET & SET
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	
+	public char getGenero() {
+		return genero;
+	}
+	
+	public void setChar(char genero) {
+		this.genero = genero;
+	}
+
+	// METODOS
+
+	
 }
